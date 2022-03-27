ecc5b62040764a75f74a5adeb4888b711a6f5038
Anvee14 <anveesharm@gmail.com>
2022-03-28 00:59:34 +0530

    'splash img'

--- project_paintball/js/paintball.js
+++ project_paintball/js/paintball.js
@@ -1,31 +1,27 @@
-AFRAME.registerComponent("paint-ball", {
-  init: function () {
-    this.throwBall();
-  },
-  throwBall: function () {
-    window.addEventListener("keydown", (e) => {
-      if (e.key == "z") {
-        var ball = document.createElement("a-entity");
-        ball.setAttribute("geometry", {
-          primitive: "sphere",
-          radius: "0.1",
-        })
-        ball.setAttribute("material", "color", "white")
-        var cam = document.querySelector("#camera")
-        pos = cam.getAttribute("position")
-        ball.setAttribute('static-body', {
-          shape: "sphere",
-          mass: "0"
-        })
-        ball.setAttribute("scale", { x: 3, y: 3, z: 3 });
-        var cam = document.querySelector("#camera");
-        ball.setAttribute('dynamic-body', {
 
-        })
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var bullet = document.createElement("a-entity");
+
+        bullet.setAttribute("geometry", {
+          primitive: "sphere",
+          radius: 0.1,
+        });
+
+        bullet.setAttribute("material", "color", "black");
+
+        var cam = document.querySelector("#camera");
+
         pos = cam.getAttribute("position");
-        ball.setAttribute("position", {
+
+        bullet.setAttribute("position", {
           x: pos.x,
-          y: pos.y - 1.2,
+          y: pos.y,
           z: pos.z,
         });
 
@@ -36,36 +32,81 @@ AFRAME.registerComponent("paint-ball", {
         camera.getWorldDirection(direction);
 
         //set the velocity and it's direction
-        ball.setAttribute("velocity", direction.multiplyScalar(-20));
+        bullet.setAttribute("velocity", direction.multiplyScalar(-20));
 
         var scene = document.querySelector("#scene");
-        ball.addEventListener("collide", this.removeBall)
 
-        scene.appendChild(ball);
+        //set the bullet as the dynamic entity
+        bullet.setAttribute("dynamic-body", {
+          shape: "sphere",
+          mass: "0",
+        });
+        bullet.setAttribute("visible", false);
+
+        //add the collide event listener to the bullet
+        bullet.addEventListener("collide", this.removeBullet);
+
+        scene.appendChild(bullet);
+
+        //shooting sound
+        this.shootSound();
       }
     });
   },
-  removeBall: function (e) {
-    var eltar = e.detail.target.el;
-    var elhit = e.detail.body.el;
-    console.log(e.detail.target.el)
-    console.log(e.detail.body.el)
-   
-
-    eltar.setAttribute("velocity",  )
-
-    /*if (elhit.id.includes("pin")) {
-      elhit.setAttribute("material", {
-        opacity: 1,
-        transparent: true
-      })
-    }
-    eltar.removeEventListener("collide", this.throwBall)
-    //impulse and point vector 
-    var impulse = new CANNON.Vec3(-1, 2, 1);
-    var worldPoint = new CANNON.Vec3().copy(elhit.getAttribute("position"));
-    elhit.body.applyImpulse(impulse, worldPoint);
+  removeBullet: function (e) {
     var scene = document.querySelector("#scene");
-    scene.remove(eltar)*/
-  }
-})
+    //bullet element
+    var element = e.detail.target.el;
+
+    //element which is hit
+    var elementHit = e.detail.body.el;
+
+    //Create paint splash
+    var paint = document.createElement("a-entity");
+    var pos = element.getAttribute("position")
+    var rotate = elementHit.getAttribute("rotation")
+
+    paint.setAttribute("position", {
+      x: pos.x,
+      y: pos.y,
+      z: pos.z,
+    });
+    paint.setAttribute("rotation", {
+      x: rotate.x,
+      y: rotate.y,
+      z: rotate.z,
+    });
+    paint.setAttribute("scale", {
+      x: 2,
+      y: 2,
+      z: 2,
+    });
+
+
+    var colorNum = parseInt(Math.random() * 8 + 1)
+
+    paint.setAttribute("material", {
+      opacity: 1,
+      transparent: true,
+      src: "./images/paint splash-0" + colorNum + ".png",
+    });
+
+    paint.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.5,
+      height: 0.5
+    });
+    scene.appendChild(paint)
+
+    //remove event listener
+    element.removeEventListener("collide", this.removeBullet);
+
+    //remove the bullets from the scene      
+    scene.removeChild(element);
+  },
+  shootSound: function () {
+    var entity = document.querySelector("#sound1");
+    entity.components.sound.playSound();
+  },
+});
+
